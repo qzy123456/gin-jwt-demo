ee321c293b451c8a850ea0319b7c065e02dfb372
qichaoyang <chaoyang.qi@mybogames.com>
2021-08-02 16:48:58 +0800

    xxx

--- model/Operation.go
+++ model/Operation.go
@@ -1,6 +1,8 @@
 package model
 
-
+//Updated可以让您在记录插入或每次记录更新时自动更新数据库中的标记字段为当前时间，
+//需要在xorm标记中使用updated标记，如下所示进行标记，
+// 对应的字段可以为time.Time或者自定义的time.Time或者int,int64等int类型。
 type Operation struct {
 	Id int
 	Ip string
@@ -8,5 +10,5 @@ type Operation struct {
 	Path string
 	Body string
 	Response string
-	CreateTime int64 `xorm:"createTime"`
+	CreateTime int64 `xorm:"updated"`
 }
